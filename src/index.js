925319ca39a71ac504c2a3b399bccd70ad244336
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-03-22 23:22:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+const form = document.querySelector("form")
+const taskList = document.getElementById("tasks")
+  
 document.addEventListener("DOMContentLoaded", () => {
-  let form = document.querySelector("form");
 
-  form.addEventListener("submit", (event) => {
-    console.log(event.input)
-    event.preventDefault();
-  });
+  form.addEventListener("submit", handleSubmit);
 });
+
+function handleSubmit(event){
+  event.preventDefault()
+  const newTask = document.getElementById("new-task-description").value
+  taskList.innerHTML += `<li>${newTask}</li>`
+}
